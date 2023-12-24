bc57b1409545b1afbf35d6874098add10861319e
Pedro Folloni Pesserl <fpesserl7@gmail.com>
2023-12-23 23:07:49 -0300

    remove unnecessary files

--- include/physics.h
+++
@@ -1,7 +0,0 @@
-#ifndef PHYSICS_H_
-#define PHYSICS_H_
-
-// Linearly interpolates between a and b with constant t in the interval [0,1]
-float lerp(float a, float b, float t);
-
-#endif // PHYSICS_H_
--- src/physics.c
+++
@@ -1,6 +0,0 @@
-#include <math.h>
-#include "raylib.h"
-
-float lerp(float a, float b, float t) {
-    return (1 - t)*a + t*b;
-}
